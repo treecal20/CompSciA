7c39bc48e46352b6083831da013d42e7cdf1fce1
treecal20 <treecal20@gmail.com>
2018-09-12 13:34:41 -0700

    Initial commit

---
+++ src/test/Example.java
@@ -0,0 +1,11 @@
+package test;
+
+public class Example {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+		System.out.println("50 - 3");
+		System.out.println(8 + 6 * -2 + 4 + "0" + (2 + 5));
+	}
+
+}
